bb10455b63f6a2b3cfb0adcdd601fd5f14e95947
Matthias Griebl <matthias.griebl@outlook.de>
2017-11-20 16:43:07 +0100

    Implemented basic logic

--- ActionCenter/Program.cs
+++ ActionCenter/Program.cs
@@ -1,15 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ActionCenter
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            using (var icon = new NotifyIcon())
+            {
+                icon.Text = Resources.ActionCenter;
+
+                icon.ContextMenu = new ContextMenu(new []
+                {
+                    new MenuItem(Resources.ShowActionCenter, (s, e) => Process.Start("ms-actioncenter://")),
+                    new MenuItem(Resources.Exit, (s, e) => Application.Exit()) 
+                });
+
+                icon.Visible = true;
+                Application.Run();
+            }
         }
     }
 }
